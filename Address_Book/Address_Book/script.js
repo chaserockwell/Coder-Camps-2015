e9d87d05135952752bc7864a313da5fc614178b0
Chase Kitteridge <chase.kitteridge@gmail.com>
2015-01-30 10:45:59 -0600

    Address Book App v1.2
    Added edit and delete functionality.

--- Address_Book/Address_Book/script.js
+++ Address_Book/Address_Book/script.js
@@ -22,7 +22,24 @@ var ContactList = {};
 ContactList.storage = [];
 
 // Variable for Firebase url
-ContactList.url = 'https://c-rockwell-contact-list.firebaseio.com/';
+ContactList.baseUrl = 'https://c-rockwell-contact-list.firebaseio.com/';
+
+// Id for items to be edited
+ContactList.editId = '';
+
+// Variable used to edit the selected Contact
+ContactList.currContact;
+ContactList.currContactId;
+ContactList.editUrl;
+
+// Url creator helper function
+ContactList.urlMaker = function (id) {
+    var url = ContactList.baseUrl;
+    if (id) {
+        url += (id + "/");
+    }
+    return url + ".json";
+}
 
 // Object Construcor for contacts
 ContactList.contact = function (firstName, lastName, phone, city, email, group) {
@@ -36,14 +53,14 @@ ContactList.contact = function (firstName, lastName, phone, city, email, group) 
 }
 
 
-
+// CREATE creates new contact from input and sends it to database
 ContactList.createNew = function () {
     var fName = $('#inputFirstName').val();
     var lName = $('#inputLastName').val();
     var phone = $('#inputPhone').val();
     var city = $('#inputCity').val();
     var email = $('#inputEmail').val();
-    var tempGroup = document.getElementById("inputGroup")
+    var tempGroup = document.getElementById("inputGroup");
     var group = tempGroup.options[tempGroup.selectedIndex].text;
     if (fName === "") {
         alert("Please enter a first name");
@@ -52,28 +69,39 @@ ContactList.createNew = function () {
         alert("Please enter a last name");
         return false;
     }
-
     var tempContact = new ContactList.contact(fName, lName, phone, city, email, group);
-
-    var request = new XMLHttpRequest();
-    request.open('POST', ContactList.url + ".json", true);
-    request.onload = function () {
-        if (this.status >= 200 && this.status < 400) {
-            var id = JSON.parse(this.response);
-            tempContact.id = id;
-            ContactList.storage.push(tempContact);
-            ContactList.display();
-        }
-    }
-    request.send(JSON.stringify(tempContact));
-
+    ContactList.post(tempContact);
 }
 
-// Display contacts to table ///////////////////////// Experiment
+// Send POST to firebase
+ContactList.post = function (dataToSend) {
+    var url = ContactList.urlMaker();
+    var request = new XMLHttpRequest();
+    request.open('POST', url, true);
+    request.onload = function () {
+        if (this.status >= 200 && this.status < 400) {
+            console.log('POST was successful!');
+            var id = JSON.parse(this.response);
+            dataToSend.id = id;
+            ContactList.storage.push(dataToSend);
+            ContactList.retrieve();
+        } else {
+            console.log('POST was unsuccessful.');
+        }
+    }
+
+    request.send(JSON.stringify(dataToSend));
+}
+
+
+// Display contacts to table 
 ContactList.display = function () {
+    var counter = 0,
+        idStr;
     var table = document.getElementById("tableBody");
     table.innerHTML = "";
     for (var i in ContactList.storage) {
+        idStr = i;
         var row = table.insertRow(i);
         var name1 = row.insertCell(0);
         name1.innerHTML = ContactList.storage[i].firstName;
@@ -87,30 +115,97 @@ ContactList.display = function () {
         city.innerHTML = ContactList.storage[i].city;
         var group = row.insertCell(5);
         group.innerHTML = ContactList.storage[i].group;
+        var btns = row.insertCell(6);
+        btns.innerHTML = "<button type='submit' class='btn btn-default formBtn' value='' onclick='ContactList.update(" + i + ")' data-toggle='modal' data-target='#myModal' ><span class='glyphicon glyphicon-pencil' aria-hidden='true' ></span></button>";
+        btns.innerHTML += "<button type='submit' class='btn btn-default formBtn' value='' onclick='ContactList.delete(" + i + ")'><span class='glyphicon glyphicon-remove' aria-hidden='true'></span></button>";
     }
+
 }
 
-
-
-
-// Get contacts from Firebase
+// READ Get contacts from Firebase
 ContactList.retrieve = function () {
+    var url = ContactList.urlMaker();
     var request = new XMLHttpRequest();
-    request.open('GET', ContactList.url + ".json", true);
+    request.open('GET', url, true);
     request.send();
     request.onload = function () {
         if (this.status >= 200 && this.status < 400) {
             var data = JSON.parse(request.response);
+            console.log('GET was successful');
+            ContactList.storage = [];
             for (var i in data) {
                 data[i].id = i;
                 ContactList.storage.push(data[i]);
             }
-            console.log(ContactList.storage[0].id);
             ContactList.display();
         } else {
             console.log("Error");
         }
     }
+}
+
+ContactList.update = function (itemIndex) {
+    currContact = ContactList.storage[itemIndex];
+    currContactId = currContact.id;
+    editUrl = ContactList.urlMaker(currContactId);
+
+    $('#modalFirstName').val(currContact.firstName);
+    $('#modalLastName').val(currContact.lastName);
+    $('#modalPhone').val(currContact.phone);
+    $('#modalCity').val(currContact.city);
+    $('#modalEmail').val(currContact.email);
+    $('#modalGroup').val(currContact.group);
+}
+
+ContactList.saveChanges = function () {
+    var fName = $('#modalFirstName').val();
+    var lName = $('#modalLastName').val();
+    var phone = $('#modalPhone').val();
+    var city = $('#modalCity').val();
+    var email = $('#modalEmail').val();
+    var tempGroup = document.getElementById("modalGroup");
+    var group = tempGroup.options[tempGroup.selectedIndex].text;
+
+    var tempContact = new ContactList.contact(fName, lName, phone, city, email, group);
+    ContactList.put(tempContact);
+}
+
+// UPDATE Update contacts in Firebase
+ContactList.put = function (itemToEdit) {
+    
+    var request = new XMLHttpRequest();
+    request.open('PUT', editUrl, true);
+    request.onload = function () {
+        if (this.status >= 200 && this.status < 400) {
+            console.log("PUT was successful!");
+            ContactList.retrieve();
+        } else {
+            console.log("PUT was unsuccessful.");
+        }
+    }
+    
+    ContactList.retrieve();
+    var pItemToEdit = JSON.stringify(itemToEdit);
+    request.send(pItemToEdit);
+}
+
+// DELETE Delete contacts from Firebase
+ContactList.delete = function (itemToDelete) {
+    var currContact = ContactList.storage[itemToDelete];
+    var currContactId = currContact.id;
+    deleteUrl = ContactList.urlMaker(currContactId);
+    var request = new XMLHttpRequest();
+    request.open('DELETE', deleteUrl, true);
+    request.onload = function () {
+        if (this.status >= 200 && this.status < 400) {
+            console.log("DELETE was successful!");
+            ContactList.retrieve();
+        } else {
+            console.log("DELETE was unsuccessful.");
+        }
+    }
+    
+    request.send();
 }
 
 ContactList.retrieve();
---
+++ Callbacks/Callbacks/script.js
@@ -0,0 +1,175 @@
+﻿//#0
+// Write a function that makes an AJAX GET call to a firebase location of your choice 
+// (the firebase should have some data). The function should have a parameter called callback. 
+// In the onload and onerror methods of the request ask the callback to run and pass
+// it the data that came back from the get call 
+// Example: callback(this.response) 
+// Call the function and pass it a parameter of alert. It should look something like this: 
+// myAJAX(alert); 
+
+var ajax = {};
+ajax.url = 'https://rockwell-ajaxtest.firebaseio.com/.json'
+
+ajax.makeCall = function (callback) {
+    var request = new XMLHttpRequest();
+    request.open('GET', ajax.url, true);
+    request.send();
+    request.onload = function () {
+        if (this.status >= 200 && this.status < 400) {
+            var data = JSON.parse(this.response);
+            callback(data.name);
+        }
+    }
+    request.onerror = function () {
+        callback(data.name);
+    }
+}
+
+ajax.makeCall(alert);
+
+//#1
+// Create a function that takes a string as a parameter and then writes the 
+// value of that parameter out to a div of your choice. 
+// Call the AJAX function you wrote for 0 and pass in your new divWriter function as the parameter, i.e.: 
+// myAJAX(divWriter); 
+
+ajax.displayString = function (string) {
+    var div = document.getElementById("main-div");
+    var par = document.createElement("p");
+    par.className = 'par';
+    div.appendChild(par);
+    var text = document.createTextNode(string);
+    par.appendChild(text);
+}
+
+ajax.makeCall(ajax.displayString);
+
+//#2
+// Call your ajax function again, but pass in an anonymous function that runs both alert and divWriter
+// as a callback to the AJAX function. Don't forget to add a parameter to the anonymous function. 
+
+ajax.makeCall(function (data) {
+    alert(data);
+    ajax.displayString(data);
+});
+
+//#3
+// Modify your AJAX call to take 2 parameters, call them success and error. Run the success callback
+// in the portion of the code that runs if the call was successful. 
+// Run the error callback where there was an error. 
+// Call your AJAX and Pass in alert on the error and divWriter as the success callback. 
+
+ajax.makeCall_2 = function (success, error) {
+    var request = new XMLHttpRequest();
+    request.open('GET', ajax.url, true);
+    request.send();
+    request.onload = function () {
+        if (this.status >= 200 && this.status < 400) {
+            var data = JSON.parse(this.response)
+            success(data.name);
+        }
+    }
+    request.onerror = function () {
+        error(data.name);
+    }
+}
+
+ajax.makeCall_2(ajax.displayString, alert);
+
+//#4
+// Call your AJAX pass in an anonymous function that outputs errors to the console.
+// Write the response to a div if the call is successful
+
+ajax.makeCall_2(ajax.displayString, function (data) {
+    console.log(data);
+});
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                 PRACTICE                                                                      //
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//var allUserData = [];
+
+//function logStuff(userData) {
+//    if (typeof userData === "string") {
+//        console.log(userData);
+//    } else if (typeof userData === "object") {
+//        for (var item in userData) {
+//            console.log(item + ": " + userData[item]);
+//        }
+//    }
+//}
+
+//function getInput(options, callback) {
+//    allUserData.push(options);
+
+//    if (typeof callback === "function") {
+
+//        callback(options);
+//    }
+
+//}
+
+//var data = {
+//    name: "Chase",
+//    age: "24",
+//    gender: "male"
+//}
+
+//var strData = "Hello World";
+//getInput(data, logStuff);
+//getInput(strData, logStuff);
+
+//var clientData = {
+//    id: 094545,
+//    fullName: "Not Set",
+//    setUserName: function (firstName, lastName) {
+//        this.fullName = firstName + " " + lastName;
+//    }
+//}
+
+
+
+//getUserInput("Chase", "Kitteridge", clientData.setUserName, clientData);
+//console.log(clientData.fullName);
+//console.log(window.fullName);
+
+//var client = {
+//    id: "1234",
+//    fullName: "not set",
+//    setName: function (firstName, lastName) {
+//        console.log(firstName + " " + lastName);
+//    }
+//}
+
+//function getClientInput(firstName, lastName, callback, callbackObj) {
+//    callback.call(callbackObj, firstName, lastName);
+//}
+
+//getClientInput("Finn", "Thehuman", client.setName, client);
+
+//function genericPoemMaker(name, gender) {
+//    console.log(name + "is finer than fine wine.");
+//    console.log("Altruistic and noble for the modern time.");
+//    console.log("Always admirably adorned with the latest style.")
+//    console.log("A " + gender + " of unfortunate tragedies who still manages a perpetual smile.");
+//}
+
+//function greeter(customerName, sex) {
+//    var salutation = sex && sex === "Man" ? "Mr." : "Ms.";
+//    console.log("Greetings, " + salutation + customerName);
+//}
+
+//function getUserInput(firstName, lastName, gender, callback) {
+//    var fullName = firstName + " " + lastName;
+
+//    if (typeof callback === "function") {
+//        callback(fullName, gender);
+//    }
+//}
+
+//getUserInput("Chase", "Kitteridge", "Man", greeter);
+//getUserInput("Chase", "Kitteridge", "Female", greeter);
